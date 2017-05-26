85b729b2389c0212ad66cfa29151827543e15663
dmitriy <pecomnn@gmail.com>
2017-05-26 19:30:42 +0300

    Before deploy

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone as tz
+from . models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=tz.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
